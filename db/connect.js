2fe31b0b21c6bd8952f8d7b98b568bef0351fcf7
noels <nojelly94@gmail.com>
2022-09-17 21:37:37 -0700

    add Taskschema

--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose')
 
-const connectionString = 'mongodb+srv://noels:<password>@nodeexpressprojects.mcpgtke.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority'
 
 const connectDB = (url) => {
     return mongoose
-    .connect(connectionString,{
+    .connect(url,{
         useNewUrlParser: true,
         useCreateIndex: true,
         useFindAndModify: false,
