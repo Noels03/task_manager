8b121b1f684222e51ceb6a9f1c00c5cc0e6d8bf4
noels <nojelly94@gmail.com>
2022-09-19 17:53:33 -0700

    add error handlers

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,48 +1,41 @@
 const Task = require('../models/Task')
-const getAllTasks = async (req, res) => {
-    try {
+const asyncWrapper = require('../middleware/async')
+const { createCustomError } = require('../errors/custom-error')
+
+const getAllTasks = asyncWrapper( async (req, res) => {
         const tasks = await Task.find({})
         res.status(200).json({ tasks })
-    } catch (error) {
-        res.status(500).json({ msg: error })
-    }
-}
+       // res.status(200).json({ tasks, amount: tasks.length }) amount of tasks
 
-const createTask =  async (req, res) => {
-    try {
+      /* res
+       .status(200)
+       .json({ status: 'sucess', data: { tasks, nbHits: tasks.length} }) //flag setup */
+})
+
+const createTask = asyncWrapper( async (req, res) => {
         const task = await Task.create(req.body)
         res.status(201).json({ task })
-    } catch (error) {
-        res.status(500).json({ msg: error })
-    } 
-}
+})
 
-const getTask = async (req, res) => {
-    try {
+const getTask = asyncWrapper(async (req, res, next) => {
         const {id:taskID} = req.params
         const task = await Task.findOne({ _id:taskID });
     if (!task){
-        return res.status(404).json({ msg: `No task with id : ${taskID}` })
+        return next(createCustomError( `No task with id : ${taskID}`, 404 ))
     }
         res.status(200).json({ task })
-    } catch (error) {
-        res.status(500).json({ msg: error })
-    }
     
-}
+})
 
-const deleteTask = async (req, res) => {
-    try {
+const deleteTask = asyncWrapper(async (req, res) => {
         const {id:taskID} = req.params;
         const task = await Task.findOneAndDelete({ _id:taskID });
         if (!task) {
-            return res.status(404).json({ msg: `No task with id : ${taskID}` })
+            return next(createCustomError( `No task with id : ${taskID}`, 404 ))
         }
         res.status(200).json({ task })
-    } catch (error) {
-        res.status(500).json({ msg: error })
-    } 
-}
+
+})
 
 const updateTask = async (req, res) => {
     try {
@@ -52,9 +45,9 @@ const updateTask = async (req, res) => {
             runValidators: true,
         })
         if (!task) {
-            return res.status(404).json({ msg: `No task with id : ${taskID}` })
+            return next(createCustomError( `No task with id : ${taskID}`, 404 ))
         }
-        res.status(200).json({ })
+        res.status(200).json({ task })
     } catch (error) {
         res.status(500).json({ msg: error })
     }  
